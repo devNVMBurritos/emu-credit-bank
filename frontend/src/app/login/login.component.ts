0faf224f37f4c4401c8ca6a9bd9fa1e3adf53eb0
TheMadZergling <development.nvmburritos@gmail.com>
2022-04-28 09:43:11 +0200

    prepareing refactoring to one tap version

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { GoogleAuthService } from '../_services/google-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,24 +6,11 @@ import { GoogleAuthService } from '../_services/google-auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  private user!: gapi.auth2.GoogleUser;
 
-  constructor( private googleAuthSercie: GoogleAuthService, private ref: ChangeDetectorRef ) { }
+
+  constructor( ) { }
 
   ngOnInit(): void {
-    this.googleAuthSercie.observable().subscribe( user => {
-      console.log(user);
-      this.user = user;
-      this.ref.detectChanges();
-    });
-  }
 
-  SingIn () {
-    this.googleAuthSercie.SingIn();
   }
-
-  SingOut () {
-    this.googleAuthSercie.SingOut();
-  }
-
 } 
