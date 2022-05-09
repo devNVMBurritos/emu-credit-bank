0c7383c687e33bb94feb5359803606b8587332c0
nvmburritos <development.nvmburritos@gmail.com>
2022-05-09 19:12:32 +0200

    Connection request duplicate fix
    Its probably should be fixed in database level but for whatever reason it doesn't want to throw error on duplicate indexes.

--- backend/handlers/user/connection/request.js
+++ backend/handlers/user/connection/request.js
@@ -5,20 +5,33 @@ module.exports = async (req, res) => {
 	if (!req.body.requested) {
 		res.send(JSON.stringify('Invalid request id'));
 	}
-	Requests.findOne({
-		requested: res.locals.user._id,
-		requester: req.body.requested,
-		confirmed: false
-	}).then(request => {
+	Requests.findOne().or([
+		{
+			requested: res.locals.user._id,
+			requester: req.body.requested
+		},
+		{
+			requested: req.body.requested,
+			requester: res.locals.user._id,
+		}
+	]).then(request => {
 		if (request) {
+			if (
+				request.requester._id.toString() == res.locals.user._id.toString() 
+				|| request.confirmed == true
+			) {
+				
+				
+				throw new Error('Already created connection!');
+			}
+
 			request.confirmed = true;
 			request.save();
-
 			res.send(JSON.stringify('Request Confirmed!'));
 			return;
 		}
 
-		Requests.create({
+		return Requests.create({
 			requester: res.locals.user._id,
 			requested: req.body.requested,
 			confirmed: false
@@ -29,12 +42,9 @@ module.exports = async (req, res) => {
 			
 			request.save();
 			res.send(JSON.stringify('Request sent.'));
-		}).catch( err => {
-			res.status(400);
-			res.send(JSON.stringify(err));
 		});
+	}).catch( err => {
+		res.status(400);
+		res.send(JSON.stringify(err));
 	});
-
-
-
 };
