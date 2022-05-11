c41301ec7a45caffa55a634fc669ebaa7d2e0003
TheMadZergling <development.nvmburritos@gmail.com>
2022-05-11 13:29:14 +0200

    register frontend fix

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
@@ -10,16 +10,19 @@ import { AuthenticationService } from '../_services/authentication.service';
 })
 export class RegisterComponent implements OnInit {
   public form: FormGroup
+  public loading  = false;
+  public error!: string;
 
   constructor(
     private authService: AuthenticationService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private cd: ChangeDetectorRef
   ) { 
     this.form = formBuilder.group({
       email: ['', [ Validators.email, Validators.required ]],
       username: ['', [Validators.minLength(3), Validators.required]],
-      password: ['', [Validators.minLength(8)]]
+      password: ['', [Validators.minLength(8), Validators.required, Validators.pattern('.*[0-9].*'), Validators.pattern('.*[a-z].*'), Validators.pattern('.*[A-Z].*')]]
     });
   }
 
@@ -27,18 +30,24 @@ export class RegisterComponent implements OnInit {
   }
 
   public Register() {
+    this.loading = true;
     const controls = this.form.controls;
     this.authService.Register(
       controls.username.value,
       controls.email.value,
       controls.password.value
     ).subscribe( 
-      response => {
+      (response) => {
+        this.loading = false;
         this.router.navigate(['/login']);
-     },
-     err => {
-
-     }
+      },
+      err => {
+        this.loading = false;
+        if (typeof err.error === 'string')
+          this.error = err.error;
+        else 
+          this.error = err.message;
+      }
     );
   }
 
